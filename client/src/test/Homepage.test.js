3f1bc52d0bc62856b7d49681135e92c38e1aba96
David de Esteban <estdav@protonmail.com>
2024-01-13 10:42:05 +0100

    test: client - implement initial setup for testing

---
+++ client/src/test/Homepage.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Homepage from '../components/Homepage.jsx';
+
+test('renders content', () => {
+  render(<Homepage />);
+
+  const element = screen.getByText('Homepage');
+  expect(element).toBeDefined();
+});
