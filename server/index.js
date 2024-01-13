f5aaf14ccb44c81fcc327cab70c8f32dd18d0d81
David de Esteban <estdav@protonmail.com>
2024-01-13 10:20:07 +0100

    test: server - use test database address when using development server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ mongoose.set('strictQuery', false);
 
 require('dotenv').config();
 
-const MONGODB_URI = process.env.NODE_ENV === 'test'
+const MONGODB_URI = process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'
   ? process.env.MONGODB_URI_TEST
   : process.env.MONGODB_URI;
 
