94f47177dea39b6158d6999940493033fc1ae5f7
David de Esteban <estdav@protonmail.com>
2024-01-16 14:17:40 +0100

    feat: client - implement homepage functionality and branding

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,12 +1,49 @@
 import { Link } from 'react-router-dom';
 
-const Homepage = () => (
-  <div id="homepage" className="container">
-    Homepage
-    <Link to="/login">Login</Link>
-    <Link to="/signup">Sign Up</Link>
-    <Link to="/dashboard">Dashboard</Link>
-  </div>
-);
+const Homepage = () => {
+  const lines = [...Array(10).keys()];
+  return (
+    <div id="homepage">
+      <div id="homepage-body">
+        <div id="homepage-text">
+          <div id="homepage-text-header">
+            <div id="homepage-logo">LJ</div>
+            <div>
+              <h1 id="homepage-text-logo">LIJR</h1>
+              <div>
+                {'Journal Jour Life'.split(' ').map((word, index) => (
+                  <h2 className="homepage-text-tagline" key={index}>
+                    {word}
+                  </h2>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div id="homepage-text-descriptions"></div>
+        </div>
+        <div id="journal-container">
+          <div id="journal-outline">
+            <div className="journal-pages journal-pages-left">
+              {lines.map((line) => (
+                <div className="journal-line" key={line}></div>
+              ))}
+              <button className="journal-button">
+                <Link to="/login">Login</Link>
+              </button>
+            </div>
+            <div className="journal-pages journal-pages-right">
+              {lines.map((line) => (
+                <div className="journal-line" key={line}></div>
+              ))}
+              <button className="journal-button">
+                <Link to="/signup">Sign Up</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
